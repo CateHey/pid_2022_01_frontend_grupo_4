759a368e16a2d1f922f4f33a820deb1a8122df9a
davidhom13 <davidhugo.ortiz@gmail.com>
2022-04-27 00:26:49 -0500

    Registrar visita

--- src/app/models/visitante.model.ts
+++ src/app/models/visitante.model.ts
@@ -1,2 +1,13 @@
+import { Departamento } from "./departamento.model";
+
 export class Visitante {
+
+    nom_vis?:string;
+    ape_vis?:string;
+    fechanac_vis?: Date;
+    dni_vis?: string;
+    correo_vis?: string;
+    tel_vis?: string;
+    cod_dep?:Departamento;
+
 }
