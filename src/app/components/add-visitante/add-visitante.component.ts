cfb6c11558e320d10ec7007b0809cb9204510e6d
davidhom13 <davidhugo.ortiz@gmail.com>
2022-04-30 16:27:33 -0500

    Validaciones de registrar Visitante v1

--- src/app/components/add-visitante/add-visitante.component.ts
+++ src/app/components/add-visitante/add-visitante.component.ts
@@ -31,6 +31,9 @@ export class AddVisitanteComponent implements OnInit {
    }
 
   registraVisita(){
+    if (!this.validar()) {
+      return;
+    }
       this.visitanteService.insertaVisitante(this.visitante).subscribe(
         response => {
           console.log(response.mensaje);
@@ -40,6 +43,42 @@ export class AddVisitanteComponent implements OnInit {
             console.log(error);
         }
       );
+  }
+
+  validar(): boolean {
+    let retorno: boolean = true;
+
+    if (this.visitante.nom_vis === undefined || this.visitante.nom_vis == null || this.visitante.nom_vis == '') {
+      alert("Es requerido ingresar el nombre del visitante");
+      retorno = false;
+    }
+
+    if (this.visitante.ape_vis === undefined || this.visitante.ape_vis == null || this.visitante.ape_vis == '') {
+      alert("Es requerido ingresar el apellido del visitante");
+      retorno = false;
+    }
+
+    if (this.visitante.dni_vis === undefined || this.visitante.dni_vis == null || this.visitante.dni_vis =='') {
+      alert("Es requerido ingresar el dni del visitante");
+      retorno = false;
+    }
+
+    if (this.visitante.correo_vis === undefined || this.visitante.correo_vis == null || this.visitante.correo_vis =='') {
+      alert("Es requerido ingresar el correo electronico del visitante");
+      retorno = false;
+    }
+
+    if (this.visitante.tel_vis === undefined || this.visitante.tel_vis == null || this.visitante.tel_vis =='') {
+      alert("Es requerido ingresar el telefono del visitante");
+      retorno = false;
+    }
+
+    if (this.visitante.cod_dep?.cod_dep === undefined || this.visitante.cod_dep.cod_dep == null || this.visitante.cod_dep.cod_dep == -1) {
+      alert("Es requerido elegir un departamento");
+      retorno = false;
+    }
+
+    return retorno;
   }
 
   ngOnInit(): void {
